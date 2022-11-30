c74a1c3a4cc7b97901ed995e9d5ec3ce78b7dfde
vox09 <xuxinyuan09@outlook.com>
2022-12-01 00:14:23 +0800

    A working grid NN draft.

--- draft.py
+++ draft.py
@@ -2,58 +2,75 @@ import taichi as ti
 
 ti.init(ti.cuda, random_seed=0)
 
-n = 4
-res = 2
+n_in_dims = 2
+n_out_dims = 3
 
-vif = ti.Vector.field(2, ti.f32, shape=res, needs_grad=True)
-vof = ti.Vector.field(3, ti.f32, shape=res, needs_grad=True)
-tgt = ti.Vector.field(3, ti.f32, shape=res, needs_grad=True)
+depth = 4   # must > 0
+width = 4
+batchsize = 8
+grid_shape = (3, 3)
 
-i_w = ti.Matrix.field(n, 2, ti.f32, shape=(), needs_grad=True)
-i_b = ti.Vector.field(n, ti.f32, shape=(), needs_grad=True)
-h1_w = ti.Matrix.field(n, n, ti.f32, shape=(), needs_grad=True)
-h1_b = ti.Vector.field(n, ti.f32, shape=(), needs_grad=True)
-h2_w = ti.Matrix.field(n, n, ti.f32, shape=(), needs_grad=True)
-h2_b = ti.Vector.field(n, ti.f32, shape=(), needs_grad=True)
-o_w = ti.Matrix.field(3, n, ti.f32, shape=(), needs_grad=True)
-o_b = ti.Vector.field(3, ti.f32, shape=(), needs_grad=True)
+lr = 1e-5
+
+#### input ouput ####
+io_shpae = (batchsize, grid_shape[0], grid_shape[1])
+vif = ti.Vector.field(n_in_dims, ti.f32, shape=io_shpae, needs_grad=True)
+vof = ti.Vector.field(n_out_dims, ti.f32, shape=io_shpae, needs_grad=True)
+tgt = ti.Vector.field(n_out_dims, ti.f32, shape=io_shpae, needs_grad=True)
+#####################
+
+#### network structure #######
+# first layer
+i_w = ti.Matrix.field(width, n_in_dims, ti.f32, shape=grid_shape, needs_grad=True)
+i_b = ti.Vector.field(width, ti.f32, shape=grid_shape, needs_grad=True)
+# last layer
+o_w = ti.Matrix.field(n_out_dims, width, ti.f32, shape=grid_shape, needs_grad=True)
+o_b = ti.Vector.field(n_out_dims, ti.f32, shape=grid_shape, needs_grad=True)
+# hidden layer
+h_w_l = []
+h_b_l = []
+for i in range(depth):
+    h_w_l.append(ti.Matrix.field(width, width, ti.f32, shape=grid_shape, needs_grad=True))
+    h_b_l.append(ti.Vector.field(width, ti.f32, shape=grid_shape, needs_grad=True))
+# a temp buffer for intermediate result
+vmf = ti.Vector.field(width, ti.f32, shape=io_shpae, needs_grad=True)
 
 loss_sf = ti.field(ti.f32, shape=(), needs_grad=True)
+###############################
+
 
 @ti.func
 def relu(vec: ti.template()):
     vec *= (vec > 0)
 
+@ti.func
+def sigmoid(vec: ti.template()):
+    vec = 1.0 / (1 + ti.exp(-vec))
+
+activation = relu
 @ti.kernel
-def forward(vif: ti.template(), vof: ti.template()):
-    for I in ti.grouped(vif):
-        # 
-        v = vif[I]
-        wi = i_w[None]
-        bi = i_b[None]
-        w1 = h1_w[None]
-        b1 = h1_b[None]
-        w2 = h2_w[None]
-        b2 = h2_b[None]
-        wo = o_w[None]
-        bo = o_b[None]
-        # results
-        ri = wi @ v + bi
-        relu(ri)
-        rh = w1 @ ri + b1
-        relu(rh)
-        rh = w2 @ rh + b2
-        relu(rh)
-        ro = wo @ rh + bo
-        relu(ro)
+def forward_layer(vif: ti.template(), vof: ti.template(), wf: ti.template(), bf: ti.template()):
+    for I in ti.grouped(wf):
+        for i in range(vif.shape[0]):
+            v = vif[i, I]
+            w = wf[I]
+            b = bf[I]
+            # results
+            r = w @ v + b
+            activation(r)
         
-        vof[I] = ro
+            vof[i, I] = r
 
+def forward(vif, vof):
+    forward_layer(vif, vmf, i_w, i_b)
+    for i in range(depth):
+        forward_layer(vmf, vmf, h_w_l[i], h_b_l[i])
+    forward_layer(vmf, vof, o_w, o_b)
 
 @ti.kernel
-def l2_loss(vof: ti.template(), tgt: ti.template(), loss_sf: ti.template()):
+def l2_loss(scale: ti.f32, vof: ti.template(), tgt: ti.template(), loss_sf: ti.template()):
     for I in ti.grouped(vof):
-        loss_sf[None] += ((tgt[I] - vof[I])**2).sum()
+        loss_sf[None] += ((tgt[I] - vof[I])**2).sum() * scale
 
 
 # Potential bug...
@@ -71,66 +88,59 @@ def init_vec_random(f: ti.template()):
             f[I][n] = ti.random()
 
 @ti.kernel
-def step(lr: ti.f32):
-    for I in ti.grouped(i_w):
-        i_w[I] -= lr * i_w.grad[I]
-    for I in ti.grouped(i_b):
-        i_b[I] -= lr * i_b.grad[I]
+def step_layer(lr: ti.f32, f: ti.template()):
+    for I in ti.grouped(f):
+        f[I] -= lr * f.grad[I]
 
-    for I in ti.grouped(h1_w):
-        h1_w[I] -= lr * h1_w.grad[I]
-    for I in ti.grouped(h1_b):
-        h1_b[I] -= lr * h1_b.grad[I]
+def step(lr):
+    step_layer(lr, i_w)
+    step_layer(lr, i_b)
+    for i in range(depth):
+        step_layer(lr, h_w_l[i])
+        step_layer(lr, h_b_l[i])
+    step_layer(lr, o_w)
+    step_layer(lr, o_b)
 
-    for I in ti.grouped(h2_w):
-        h2_w[I] -= lr * h2_w.grad[I]
-    for I in ti.grouped(h2_b):
-        h2_b[I] -= lr * h2_b.grad[I]
-        
-    for I in ti.grouped(o_w):
-        o_w[I] -= lr * o_w.grad[I]
-    for I in ti.grouped(o_b):
-        o_b[I] -= lr * o_b.grad[I]
-
-
-vif[0][0] = 1.0
-vif[0][1] = -2.0
-vif[1][0] = 2.0
-vif[1][1] = -4.0
-
+#### init ####
+init_vec_random(vif)
 
 init_mat_random(i_w)
 init_vec_random(i_b)
 
-init_mat_random(h1_w)
-init_vec_random(h1_b)
-
-init_mat_random(h2_w)
-init_vec_random(h2_b)
+for i in range(depth):
+    init_mat_random(h_w_l[i])
+    init_vec_random(h_b_l[i])
 
 init_mat_random(o_w)
 init_vec_random(o_b)
+###############
 
+#### train ####
 # forward(vf)
-for i in range(100):
+n_element = io_shpae[0] * io_shpae[1] * io_shpae[2]
+for e in range(10):
     with ti.ad.Tape(loss_sf):
-        forward(vif, vof)     # call twice
-        l2_loss(vof, tgt, loss_sf)
-    step(1e-3)
-    print('==== loss ====')
+        forward(vif, vof)
+        l2_loss(1. / n_element, vof, tgt, loss_sf)
+    step(lr)
+
+    print(f'==== loss ==== epoch {e}')
     print(loss_sf[None])
 
-# print('==== weight ====')
-# print('i_w', i_w)
-# print('i_b', i_b)
-# print('h1_w', h1_w)
-# print('h1_b', h1_b)
-# print('h2_w', h2_w)
-# print('h2_b', h2_b)
-# print('==== grad ====')
-# print('i_w.grad', i_w.grad)
-# print('i_b.grad', i_b.grad)
-# print('h1_w.grad', h1_w.grad)
-# print('h1_b.grad', h1_b.grad)
-# print('h2_w.grad', h2_w.grad)
-# print('h2_b.grad', h2_b.grad)
+print('==== weight ====')
+print('i_w', i_w)
+print('i_b', i_b)
+print('o_w', o_w)
+print('o_b', o_b)
+print('==== grad ====')
+print('i_w.grad', i_w.grad)
+print('i_b.grad', i_b.grad)
+print('o_w.grad', o_w.grad)
+print('o_b.grad', o_b.grad)
+###################
+print('==============')
+print(f'depth: {depth}')
+print(f'width: {width}')
+print(f'batchsize: {batchsize}')
+print(f'grid_shape: {grid_shape}')
+print('==============')
